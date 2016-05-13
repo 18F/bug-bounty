817667719ba321ea1e12e8bc206b7419880553bf
Jacob Kaplan-Moss <jacob.kaplan-moss@gsa.gov>
2016-05-13 09:52:13 -0700

    added basic auth wrapper until we get ATO'd

--- bugbounty/wsgi.py
+++ bugbounty/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for bugbounty project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bugbounty.settings")
-
 application = get_wsgi_application()
+
+# Throw the whole site behind basic auth, if requested. This is temporary
+# until the site gets ATO'd.
+if 'WSGI_AUTH_CREDENTIALS' in os.environ:
+    import wsgi_basic_auth
+    application = wsgi_basic_auth.BasicAuth(application)
