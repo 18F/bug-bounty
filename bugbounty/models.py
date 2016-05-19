6e999eb7ccefcb36e730433dd892f12460e38b81
Jacob Kaplan-Moss <jacob.kaplan-moss@gsa.gov>
2016-05-19 14:18:32 -0700

    Use enumfields correctly.

--- bugbounty/models.py
+++ bugbounty/models.py
@@ -1,30 +1,45 @@
-import enum
 import enumfields
 from django.db import models
 from django.utils import timezone
 
-class ReportType(enum.Enum):
-    auth_fail = "Authentication bypass/failure"
-    csrf = "Cross-Site Request Forgery (CSRF)"
-    xss = "Cross-Site Scripting (XSS)"
-    crypto = "Cryptographic issue"
-    dos = "Denial of Service"
-    design = "Design Issue"
-    information_disclosure = "Information Disclosure"
-    missing_best_practice = "Missing Best Practice"
-    privesc = "Privilege Escalation"
-    rce = "Remote Code Execution"
-    sqli = "SQL Injection"
-    clickjacking = "Clickjacking"
-    redirect = "Open Redirect"
-    other = "Other"
+class ReportType(enumfields.Enum):
+    AUTH_FAIL = "auth_fail"
+    CSRF = "csrf"
+    XSS = "xss"
+    CRYPTO = "crypto"
+    DOS = "dos"
+    DESIGN = "design"
+    INFORMATION_DISCLOSURE = "information_disclosure"
+    MISSING_BEST_PRACTICE = "missing_best_practice"
+    PRIVESC = "privesc"
+    RCE = "rce"
+    SQLI = "sqli"
+    CLICKJACKING = "clickjacking"
+    REDIRECT = "open_redirect"
+    OTHER = "other"
 
-class ReportStatus(enum.Enum):
-    new = 'new'
-    in_progress = 'in progress'
-    waiting = 'waiting on reporter'
-    resolved = 'resolved'
-    invalid = 'invalid'
+    class Labels:
+        AUTH_FAIL = "Authentication bypass/failure"
+        CSRF = "Cross-Site Request Forgery (CSRF)"
+        XSS = "Cross-Site Scripting (XSS)"
+        CRYPTO = "Cryptographic issue"
+        DOS = "Denial of Service"
+        DESIGN = "Design Issue"
+        INFORMATION_DISCLOSURE = "Information Disclosure"
+        MISSING_BEST_PRACTICE = "Missing Best Practice"
+        PRIVESC = "Privilege Escalation"
+        RCE = "Remote Code Execution"
+        SQLI = "SQL Injection"
+        CLICKJACKING = "Clickjacking"
+        REDIRECT = "Open Redirect"
+        OTHER = "Other"
+
+class ReportStatus(enumfields.Enum):
+    NEW = 'new'
+    IN_PROGRESS = 'in_progress'
+    WAITING = 'waiting'
+    RESOLVED = 'resolved'
+    INVALID = 'invalid'
 
 class Report(models.Model):
     reporter_name = models.TextField()
@@ -33,7 +48,7 @@ class Report(models.Model):
     type = enumfields.EnumField(ReportType, max_length=200)
     title = models.TextField()
     details = models.TextField()
-    status = enumfields.EnumField(ReportStatus, max_length=200, default=ReportStatus.new)
+    status = enumfields.EnumField(ReportStatus, max_length=200, default=ReportStatus.NEW)
 
     date_created = models.DateTimeField(default=timezone.now)
     date_updated = models.DateTimeField(default=timezone.now)
