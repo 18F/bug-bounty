8b64688cd039e09d84510400044f0ac5709de58c
Jacob Kaplan-Moss <jacob.kaplan-moss@gsa.gov>
2016-05-20 15:18:29 -0700

    made most report fields readonly

--- bugbounty/admin.py
+++ bugbounty/admin.py
@@ -2,29 +2,22 @@ from django import forms
 from django.contrib import admin
 from .models import Report, Target
 
-class ReportAdminForm(forms.ModelForm):
-    class Meta:
-        model = Report
-        fields = '__all__'
-        widgets = {
-            'reporter_name': forms.TextInput(attrs={'size': '50'}),
-            'title': forms.TextInput(attrs={'size': '50'}),
-        }
-
 @admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
     list_display = ('title', 'date_updated', 'status', 'type')
     ordering = ('-date_updated',)
     list_filter = ('status',)
     search_fields = ('title',)
-    form = ReportAdminForm
+    readonly_fields = ('reporter_name', 'reporter_email', 'target', 'type', 'title', 'details', 'date_created', 'date_updated')
+    fields = readonly_fields + ('status',)
 
 class TargetAdminForm(forms.ModelForm):
     class Meta:
         model = Target
         fields = '__all__'
         widgets = {
-            'name': forms.TextInput(attrs={'size': '50'})
+            'name': forms.TextInput(attrs={'size': '50'}),
+            'slack_channel': forms.TextInput(attrs={'size': '50'}),
         }
 
 @admin.register(Target)
